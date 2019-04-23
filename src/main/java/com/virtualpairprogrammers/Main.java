389ccae815c5a2cc902e4401b74279574ef61307
zsayed1 <zsayed1@hawk.iit.edu>
2019-04-23 18:56:26 -0500

    Creating my first Spark Job

---
+++ src/main/java/com/virtualpairprogrammers/Main.java
@@ -0,0 +1,32 @@
+package com.virtualpairprogrammers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub\
+		List<Double> input = new ArrayList<>();
+		input.add(1.1);
+		input.add(1.2);
+		input.add(1.3);
+		input.add(1.4);
+		
+		Logger.getLogger("org.apache").setLevel(Level.WARN);
+		// Set App Name stating the Name of the app and Set master with wildcard stating the url of master
+		SparkConf conf = new SparkConf().setAppName("Starting Spark").setMaster("local[*]");
+		// Passn the conf to JavaSparkContext to read it
+		JavaSparkContext sc = new JavaSparkContext(conf);
+		// Pass the input to intialize RDD 
+		JavaRDD<Double> myrdd = sc.parallelize(input);
+		sc.close();
+	}
+
+}
