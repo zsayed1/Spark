e2ea9a5fcdfe6e8e79a044174ed76aac4b4f7b80
zeshan <zeshan@indigoconsulting.com>
2019-04-24 22:40:37 -0500

    Max number of words Solved

--- src/main/java/com/virtualpairprogrammers/ReadFromAFile.java
+++ src/main/java/com/virtualpairprogrammers/ReadFromAFile.java
@@ -1,12 +1,16 @@
 package com.virtualpairprogrammers;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+
+import scala.Tuple2;
 
 public class ReadFromAFile {
 
@@ -21,10 +25,58 @@ public class ReadFromAFile {
 		
 		JavaRDD<String> initialRDD = sc.textFile("src/main/resources/subtitles/input.txt");
 		
-		initialRDD
-			.flatMap(value -> Arrays.asList(value.split(" ")).iterator())
-			.filter(word -> word.length()>1)
-			.foreach(value-> System.out.println(value));
+//		initialRDD
+//			.flatMap(value -> Arrays.asList(value.split(" ")).iterator())
+//			.filter(word -> word.length()>1)
+//			.foreach(value-> System.out.println(value));
+	
+		//JUST TO TEST
+		//We take the initial RDD with .take(n) fucntion
+		// Save it to a JAVA Collectipn List as it spits JAVA Collection out
+//		List<String> take = initialRDD.take(50);
+		// Print the RDD with for each 
+//		take.forEach(value -> System.out.println(value));
+		
+		
+		// Removing the Anything which is not a word
+		JavaRDD<String> lettersOnlyRDD = initialRDD
+				.map(sentence -> sentence.replaceAll("[^a-zA-Z ]*", ""));
+	
+		
+		// Removing spaces from the rdd
+		JavaRDD<String> removeBlankRDD = lettersOnlyRDD
+				.map(sentence -> sentence.replaceAll("^\\s", ""));
+		
+		// Removing words which does not contain more than one character and trimming it too
+		JavaRDD<String> filteredremoveBlankRDD = removeBlankRDD
+				.filter(value->value.trim().length()>1);
+		
+//		filteredremoveBlankRDD.foreach(value-> System.out.println(value));
+		// Spliting the sentence and saving it to new RDD with String.split function
+		JavaRDD<String> filteredremoveBlankFlatRDD =  filteredremoveBlankRDD.flatMap(sentence -> Arrays.asList(sentence.split(" ")).iterator());
+		
+		// Removing words which are boring 
+		JavaRDD<String> notBoringWords= filteredremoveBlankFlatRDD
+		.filter(word -> Util.isNotBoring(word));
+		
+		 
+//		notBoringWords.foreach(value -> System.out.println(value));		
+		
+		// Mapping to pairRDD to calculate the count
+		JavaPairRDD<String, Long> conclusionToPair = notBoringWords.mapToPair(value -> new Tuple2<String, Long>(value, 1L));
+		
+		// Calculating count and saving it to new RDD
+		JavaPairRDD<String, Long> conclusion = conclusionToPair.reduceByKey((value1, value2) -> value1 + value2);
+		
+		// Switching because ByKey fucntions can be used		
+		JavaPairRDD<Long, String> switched = conclusion.mapToPair(tuple -> new Tuple2<Long,String>(tuple._2,tuple._1));
+		
+		// Using sortByKey fucntion
+		// sortByKey as false will set it to greater to lower values
+		JavaPairRDD<Long, String> sorted=switched.sortByKey(false);
+		
+		sorted.foreach(tuple -> System.out.println(tuple));
+		
 		
 		sc.close();
 	}
