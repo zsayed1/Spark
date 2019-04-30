acacb53a68841a2a6b69c1dadfde07575896de57
zeshan <zeshan@indigoconsulting.com>
2019-04-30 15:04:01 -0500

    Spark Kafka streaming getting the highest value with swap and sortbykey

--- src/main/java/com/virtualpairprogrammers/streaming/KafkaViewingPackage.java
+++ src/main/java/com/virtualpairprogrammers/streaming/KafkaViewingPackage.java
@@ -14,10 +14,13 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaInputDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.kafka010.ConsumerStrategies;
 import org.apache.spark.streaming.kafka010.KafkaUtils;
 import org.apache.spark.streaming.kafka010.LocationStrategies;
+
+import scala.Tuple2;
 
 public class KafkaViewingPackage {
 
@@ -25,7 +28,7 @@ public class KafkaViewingPackage {
 		Logger.getLogger("org.apache").setLevel(Level.WARN);
 		Logger.getLogger("org.apache.spark.storage").setLevel(Level.ERROR);
 		SparkConf conf = new SparkConf().setAppName("Kafka Streaming").setMaster("local[*]");
-		JavaStreamingContext sc = new JavaStreamingContext(conf,Durations.seconds(1));
+		JavaStreamingContext sc = new JavaStreamingContext(conf,Durations.seconds(10));
 		// Creating topics as collections to pass to consumer props
 		Collection<String> topics =Arrays.asList("test");
 		Map<String, Object> params =new HashMap<>();
@@ -41,9 +44,15 @@ public class KafkaViewingPackage {
 		 
 		 
 		JavaInputDStream<ConsumerRecord<String, String>> stream = KafkaUtils.createDirectStream(sc,  LocationStrategies.PreferConsistent(), ConsumerStrategies.Subscribe(topics, params));
-		JavaDStream<String> reading = stream.map(value-> value.value());
-		reading.print();
-		 
+//		JavaDStream<String> reading = stream.map(value-> value.value());
+		JavaPairDStream<String, Long> mapToPair = stream.mapToPair(value-> new Tuple2<>(value.value(),5L));
+		JavaPairDStream<String, Long> reduceByKey = mapToPair.reduceByKeyAndWindow((value1,value2)-> value1+value2, Durations.minutes(2));
+		// Swapp the rdd value by using swap funtion
+		JavaPairDStream<Long, String> swapped = reduceByKey.mapToPair(value-> value.swap());
+		// Using transform in javaPairDstream
+		JavaPairDStream<Long, String> transformToPair = swapped.transformToPair(rdd-> rdd.sortByKey(false));
+		
+		transformToPair.print();
 		 
 		 sc.start();
 		 sc.awaitTermination();
